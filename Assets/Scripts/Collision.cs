9d20ea97ee97a63cffcd7f0aed02e86dce217ad2
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-11-17 16:37:23 +0300

    .

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -9,15 +9,16 @@ public class Collision : MonoBehaviour
         Debug.Log("hittiing");
         Debug.Log(collision.gameObject);
         driverScript.SlowCar();
+        //gameObject.GetComponent<Driver>().SlowCar();
         Debug.Log("slower");
         // Destroy(collision.gameObject);
 
         Debug.Log(collision.gameObject);
         Debug.Log(collision.collider);
-        
+
     }
 
-    // OnTriggerEnter2D'den dönen collision parametresi direkt Collider2D bileşenidir.
+    // OnTriggerEnter2D'den dönen collider parametresi direkt Collider2D bileşenidir.
 
     // OnCollisionEnter2D'den dönen collision parametresi ise Collision2D bileşenidir. Bu bileşenden collider'a ulaşmak için collision.collider demen gerekir.
 
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -40,3 +40,43 @@ public class Trigger : MonoBehaviour
         }
     }
 }
+
+
+// normalde triggerların hepsini tek bir dosyaya toplayıp driver'a vermek yanlış. 
+// her bir ontriggerenter veya oncollisionenter olan nesneye özel ayrı bir fonksiyon yazmak lazım ki modüler olsun. 
+// mesela sadece paket için Package.cs diye bi dosya açıp böyle bişey yapmak lazımdı:
+// Package.cs (Paket prefab'ının üstündeki script)
+// using UnityEngine;
+
+// public class Package : MonoBehaviour
+// {
+//     [SerializeField] float destroySecond = 0.5f;
+
+//     // Bu script SADECE paketin görevini bilir
+//     void OnTriggerEnter2D(Collider2D other)
+//     {
+//         // 1. Bana çarpan Sürücü mü?
+//         if (other.CompareTag("Driver")) // Sürücünün etiketinin "Driver" olduğunu varsaydık
+//         {
+//             // 2. Sürücünün script'ine eriş
+//             Driver driver = other.GetComponent<Driver>(); (bu arada zaten bu scripti koyduğumuz şey için direkt GetComponent yazmamız yeterli, other. yapmya gerek yok)
+
+//             // 3. Sürücünün zaten paketi yoksa...
+//             if (driver != null && !driver.PaketiVarMi())
+//             {
+//                 Debug.Log("paket alındı");
+
+//                 // 4. Sürücü'ye "paketi al" komutunu yolla
+//                 driver.AlPaketi();
+
+//                 // 5. Kendini (paketi) yok et
+//                 Destroy(gameObject, destroySecond); (other.gameObject diyince buna çarpan nesne, sadece gameObject diyince bu scripte sahip olan nesne)
+//             }
+//         }
+//     }
+// }
+// gameObject. diyince o nesneye erişiyoruz, gameObject.GetComponent() diyince o nesnenin inspector penceresindeki componentlere erişiyoruz
+
+// gameObject: Dediğinde, o script'in bağlı olduğu nesnenin (GameObject'in) kendisine erişirsin. Bu tam olarak doğru. Bu "konteynerin" kendisidir.
+
+// gameObject.GetComponent<BileşenAdı>(): Dediğinde, o nesnenin Inspector penceresindeki component'lerin hepsine birden değil, istediğin spesifik bir tanesine erişirsin.
